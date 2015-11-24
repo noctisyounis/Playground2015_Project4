cf8e99defcfef8201d55d15a05c12a60ea4c125a
damgau <damien.gauthier3003@gmail.com>
2015-11-24 16:12:05 +0100

    Inventary
    Starter

---
+++ Assets/Scripts/Inventaire/InventoryBehaviour.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InventaryBehaviour : MonoBehaviour 
+{
+	#region Public variable
+	private int m_rowLength = 3;
+
+	#endregion
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
