58de2754f618608e69cd6eb75e6c56ef13c928dc
damgau <damien.gauthier3003@gmail.com>
2015-11-25 09:25:55 +0100

    Inventary starter

---
+++ Assets/Scripts/Inventaire/BigRangeCategory.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BigRangeCategory : AllCategory 
+{
+
+	/**
+	 * Select bigRange
+	 */
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
