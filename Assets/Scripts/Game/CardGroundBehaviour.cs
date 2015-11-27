9454120995d1d1ca33f3ae9835af191fb3c8939a
Syeste <neosoul@hotmail.fr>
2015-11-27 11:28:29 +0100

    Fix bug Viewer

--- Assets/Scripts/Game/CardGroundBehaviour.cs
+++ Assets/Scripts/Game/CardGroundBehaviour.cs
@@ -4,7 +4,13 @@ using System.Collections;
 public class CardGroundBehaviour : CardBehaviour {
 
 	#region Public Variable
-	
+
+	public Sprite m_mountain;
+	public Sprite m_swamp;
+	public Sprite m_ruin;
+	public Sprite m_plain;
+	public Sprite m_forest;
+
 	#endregion
 	
 	#region Main Methodes
