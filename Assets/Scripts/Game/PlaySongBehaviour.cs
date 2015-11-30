7e123f6a9bb1ea0946e916f6f723f892d19041f7
beartrackers <nolf199@gmail.com>
2015-11-30 13:40:19 +0100

    son

---
+++ Assets/Scripts/Game/PlaySongBehaviour.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlaySongBehaviour : MonoBehaviour {
+	
+	
+
+    #region Public Variable
+    
+    public AudioClip m_son;
+
+    #endregion
+
+    #region Main Methodes
+
+    void start()
+    {
+       
+    
+        audio = GetComponent<AudioSource>();
+    
+
+    
+        audio.PlayOneShot(m_son);
+       
+    
+    }
+
+    #endregion
+
+    #region Utils
+
+    
+
+    #endregion
+
+    #region Private Variable
+    private AudioSource audio;
+    #endregion
+}
