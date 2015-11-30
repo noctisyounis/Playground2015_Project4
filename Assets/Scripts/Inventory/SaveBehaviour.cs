8f923edf7e024680d744f78ca0c9338cefd4a8c7
damgau <damien.gauthier3003@gmail.com>
2015-11-30 13:55:55 +0100

    XML - Save
    Starter

--- Assets/Scripts/Inventory/SaveBehaviour.cs
+++ Assets/Scripts/Inventory/SaveBehaviour.cs
@@ -1,10 +1,15 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Xml;
+using System.Xml.Serialization;
 
 public class SaveBehaviour : MonoBehaviour {
 
 	public void OnMouseUp()
 	{
-
+		// if deck is full (23) : 18 units/5 lands
+		if (DeckBehaviour.m_deck.Count == 23) {
+				
+		}
 	}
 }
---
+++ Assets/Scripts/Inventory/XML/card.cs
@@ -0,0 +1,10 @@
+﻿using System.Xml;
+using System.Xml.Serialization;
+
+public class card
+{ 
+	
+	public string type;
+	
+	public int id;
+}
