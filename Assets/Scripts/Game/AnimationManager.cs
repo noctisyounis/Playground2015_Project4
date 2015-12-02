ebaaeb9d99d52fcea76bb2ee058128f9a9f0361c
Ricco7La <emeric.beauchot@gmail.com>
2015-12-02 13:12:34 +0100

    Prefab Anim Sword

---
+++ Assets/Scripts/Game/AnimationManager.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public static class AnimationManager
+{
+
+	#region Public Variable
+
+	#endregion
+	
+	#region Main Methodes
+
+	public static Animation SwordAttack(int x, int y )
+	{
+		float FloatX = (float)(5-x);
+		float FloatY = (float)(y); 
+
+		//Correction Position 3D
+		if (x > 3) 
+		{
+			FloatX += 0.05f * (x-3);
+		}
+		else if (x < 2) 
+		{
+			FloatX += 0.05f * (2-x);
+		}
+
+
+		Vector3 Position = new Vector3(FloatX,0f, FloatY);
+		Quaternion Rotation = new Quaternion(315f,0f,0f,0f);
+		GameObject prefab = (GameObject) Resources.Load("SwordAnim");
+		GameObject holder = (GameObject) GameObject.Instantiate(prefab);
+		Animation sword = holder.GetComponentInChildren<Animation>();
+		holder.transform.position = Position;
+
+		return sword;
+
+	}
+
+
+	#endregion
+	
+	#region Utils
+
+  
+
+
+	#endregion
+	
+	#region Private Variable
+
+
+
+	#endregion
+
+}
