3f63ec558a8436d3e6c15d7cef3ccf42c774c858
Syeste <neosoul@hotmail.fr>
2015-12-03 16:40:40 +0100

    Inventaire

--- Assets/Scripts/Inventory/AllCategory.cs
+++ Assets/Scripts/Inventory/AllCategory.cs
@@ -2,62 +2,16 @@
 using System.Collections;
 
 public class AllCategory : MonoBehaviour {
-
-	#region Public Variable
+	public GameObject m_Inventory;
 	
-	//private ArrayList<Carte> carts;
-
-	/*
-	 * carte.type 
-	 * -> Fabien
-	 */
-
-	/*
-	#region Utils
-	
-	private void LoadDeck()
+	void Start()
 	{
-		GameObject prefab = (GameObject)Resources.Load ("Card", typeof(GameObject));
-		// change deck by all cards
-		GameObject deck = GameObject.Find("PlayerDeck");
-		GameObject Card;
-		m_deck.Clear();
-		for (int i = 1; i < 19; i++) 
-		{
-			Card = (GameObject)Instantiate(prefab);
-			Card.transform.SetParent(deck.transform);
-			Card.name = "Card" + i.ToString();
-			m_deck.Add(Card);
-		}
-		Shuffle(m_deck);
+		m_Inventory = GameObject.FindObjectOfType<InventoryBehaviour>().gameObject;
 	}
-	
-	public void Draw()
+	public void OnMouseUp()
 	{
-		if (m_deck.Count > 0) 
-		{
-			GameObject Card = m_deck [0];
-			m_deck.Remove (Card);
-			Card.transform.SetParent (gameObject.transform);
-			Card.GetComponent<RectTransform>().localScale = new Vector3(1, 1, 1);
+		for (int i = 0; i < InventoryBehaviour.m_storage.transform.childCount; i++) {
+			InventoryBehaviour.m_storage.transform.GetChild(0).transform.SetParent(m_Inventory.transform);
 		}
-	} 
-	#endregion
-	*/
-
-	#endregion
-
-	// 1.  through the list and display them in the game object
-
-	// 2. simple click = drop to DeckList
-
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
 	}
 }
