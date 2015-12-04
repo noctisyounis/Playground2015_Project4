ff46fa504a34e01541236ae2eaf932dbb287ff95
beartrackers <nolf199@gmail.com>
2015-12-04 11:12:33 +0100

    tuto

---
+++ Assets/Scripts/tuto/nextBehaviour.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class nextBehaviour : MonoBehaviour {
+    public string m_button;
+    public int m_nbreSlide = 4;
+    public GameObject m_slide;
+    private int m_currentSlide;
+    public void onClick()
+    {
+        if (m_button == "suivant")
+        {
+            if (m_currentSlide < m_nbreSlide)
+            {
+            iTween.MoveTo(m_slide, new Vector3(),1f);
+            m_currentSlide++;
+            }
+        }
+        else
+        {
+            if (m_currentSlide > m_nbreSlide)
+            {
+                iTween.MoveTo(m_slide, new Vector3(), 1f);
+                m_currentSlide--;
+            }
+        }
+    }
+}
