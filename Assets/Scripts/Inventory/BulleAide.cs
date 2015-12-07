eef95a266af5637d8a136c2e027e72f9b558262e
damgau <damien.gauthier3003@gmail.com>
2015-12-07 10:30:49 +0100

    Inventory Final version

---
+++ Assets/Scripts/Inventory/BulleAide.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BulleAide : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Invoke("DestroyObject",7f);
+	}
+	void DestroyObject()
+	{
+		Destroy(gameObject);
+	}
+}
