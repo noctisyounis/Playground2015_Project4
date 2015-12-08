626959a9368e65113984ac139b90327e981d80b1
Ricco7La <emeric.beauchot@gmail.com>
2015-12-08 09:59:10 +0100

    Sound Tutorial And Inventory

--- Assets/Scripts/Tutorial/buttonTutorialBehaviour.cs
+++ Assets/Scripts/Tutorial/buttonTutorialBehaviour.cs
@@ -1,14 +1,44 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
+using System.Collections.Generic;
 
 public class buttonTutorialBehaviour : MonoBehaviour {
 
 	public GameObject panelMain;
 	public string type;
 
+	void FixedUpdate()
+	{
+		int id = panelMain.GetComponent<PanelHolderBehaviour>().currentPanel.GetComponent<PanelTutorialBehaviour> ().id;
+		if (type == "Right") 
+		{
+			if (id == panelMain.GetComponent<PanelHolderBehaviour>().listPanel.Count) 
+			{
+				gameObject.GetComponent<Button>().interactable = false;
+			}
+			else 
+			{
+				gameObject.GetComponent<Button>().interactable = true;
+			}
+		}
+		if (type == "Left") 
+		{
+			if (id == 1) 
+			{
+				gameObject.GetComponent<Button>().interactable = false;
+			}
+			else 
+			{
+				gameObject.GetComponent<Button>().interactable = true;
+			}
+		}
+	}
+
 	public void Onclick()
 	{
 		panelMain.GetComponent<PanelHolderBehaviour> ().switchPanel (type);
+		GetComponent<AudioSource>().Play();
 	}
 
 }
